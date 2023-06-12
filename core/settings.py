139f9f6b7c659cc68a86ef8c3577bdc032bcb2f5
eminberkeaydin <emin@authic.io>
2023-06-12 16:08:11 +0300

    New cors origin whitelist added

--- core/settings.py
+++ core/settings.py
@@ -61,8 +61,9 @@ MIDDLEWARE = [
 CORS_ORIGIN_ALLOW_ALL = True 
 
 CORS_ORIGIN_WHITELIST = [
-'http://localhost:3000',
-'http://127.0.0.1:3000'
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://calliope-api-g72rpkruuq-uc.a.run.app',
 ]
 
 ROOT_URLCONF = 'core.urls'
